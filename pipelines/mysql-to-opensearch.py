661db02f0f43f2c8865227c2dbeba0335f01db8d
Nick <91033069+nicholas-buscaglia@users.noreply.github.com>
2023-05-24 17:06:19 -0400

    Update mysql-to-opensearch.py

--- pipelines/mysql-to-opensearch.py
+++ pipelines/mysql-to-opensearch.py
@@ -10,7 +10,6 @@ es_url = os.environ['ES_URL'] + '_bulk'  # Upload multiple documents
 es_username = os.environ['ES_USERNAME']
 es_password = os.environ['ES_PASSWORD']
 
-
 def connect_to_mysql(db_host, db_user, db_password, db_name):
     return pymysql.connect(
         host=db_host,
@@ -88,20 +87,23 @@ def execute_curl_command(es_url, es_username, es_password, schema, index_name, s
     response = os.popen(f'curl -XPOST -u "{es_username}:{es_password}" {es_url} --data-binary "@{filename}" -H "Content-Type: application/json"')
     response_text = response.read()
     response.close()
-
-    if '429' in response_text:
+    if 'errors":false' in response_text:
+        pass
+    else:
         retry_count = 1
         while retry_count <= 3:
-            print('Too many requests, retrying in 3 seconds...')
+            print('error found!')
+            if retry_count==1:
+                print(f'response text: {response_text}')
             time.sleep(3)
             response = os.popen(f'curl -XPOST -u "{es_username}:{es_password}" {es_url} --data-binary "@{filename}" -H "Content-Type: application/json"')
             response_text = response.read()
             response.close()
-            if '429' not in response_text:
+            if 'errors":false' in response_text:
                 print(f'success')
                 break
             retry_count += 1
-            if retry_count == 3 and '429' in response_text:
+            if retry_count == 3 and 'errors":false' not in response_text:
                 print(f'retry attempts failed, log and move on')
                 log_message = f"Date:{datetime.datetime.now()}, Index:{index_name}, Query_Params:{str(start)},{str(step)}\n"
                 try:
@@ -120,6 +122,11 @@ def main(host, user, passwd, schema):
     mydb = connect_to_mysql(host, user, passwd, schema)
     # Query for list of tables names or change to req list here
     table_names = get_table_names(mydb)
+    # list of preferred order
+    priority_tables = ['Buffalo', 'St_Pete', 'Tampa', ]
+
+    # sort table_names based on whether they're in priority_tables
+    table_names = sorted(table_names, key=lambda x: priority_tables.index(x) if x in priority_tables else len(priority_tables))
     print(f'table_names: {table_names}')
 
     for table in table_names:
@@ -132,14 +139,14 @@ def main(host, user, passwd, schema):
         uid_index = get_index(columns, 'uid')
         # Initialize variables for documents/upload
         start = 0
-        interval = 50 # Good for rough results!
+        # interval = 50 # Good for rough results!
         # interval = 5  # Good for fine results!
         # interval = 500  # Good for rough reviews!
-        # interval = 50  # Good for fine reviews?
+        interval = 100 # Good for fine reviews
         while True:
             rows = execute_query(mydb, table, start, interval)
             if not rows:
-                break  
+                break
             write_rows_to_file(index_name, columns, rows, uid_index)
             execute_curl_command(es_url, es_username, es_password, schema, index_name, start, interval)
             start += interval
